ec8112478bb70e6c626072525bbce3002380f72a
[dyad] <huulong111@gmail.com>
2025-09-03 18:38:24 +0700

    [dyad] Tăng kích thước logo - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 export const Logo = () => (
   <div className="flex items-center">
-    <img src="/logokocao.png" alt="Logo" className="h-8 w-auto" />
+    <img src="/logokocao.png" alt="Logo" className="h-10 w-auto" />
   </div>
 );
