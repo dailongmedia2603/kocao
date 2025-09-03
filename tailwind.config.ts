683a05f6bae8fc338505d94eabfa0ce57efb774b
[dyad] <huulong111@gmail.com>
2025-09-03 19:28:15 +0700

    [dyad] Cập nhật font chữ và in đậm menu - wrote 3 file(s)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ["Golos Text", ...fontFamily.sans],
+        sans: ["Inter", ...fontFamily.sans],
       },
       colors: {
         border: "hsl(var(--border))",
