1c45412c840ac2f5c029723a30de171485bbe5bd
[dyad] <huulong111@gmail.com>
2025-10-22 09:18:01 +0700

    [dyad] Tạo trang Dashboard với thống kê và danh sách KOC - wrote 5 file(s), added @supabase/supabase-js, date-fns package(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
 import Dashboard from "./pages/Dashboard";
-import Settings from "./pages/Settings";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { SessionContextProvider } from "./contexts/SessionContext";
-import { Toaster } from "sonner";
-import ListKoc from "./pages/ListKoc";
-import KocDetail from "./pages/KocDetail";
-import KocLayout from "./components/koc/KocLayout";
-import Reports from "./pages/Reports";
-import CreateVoicePage from "./pages/CreateVoice";
-import TaoContent from "./pages/TaoContent";
-import Automation from "./pages/Automation";
-import TaoVideo from "./pages/TaoVideo";
-import VideoToScript from "./pages/VideoToScript";
-import AutomationDetail from "./pages/AutomationDetail";
-import ForgotPassword from "./pages/ForgotPassword";
-import PendingApproval from "./pages/PendingApproval";
-import AdminRoute from "./components/AdminRoute";
-import UserManagement from "./pages/admin/UserManagement";
 
 function App() {
   return (
     <BrowserRouter>
-      <SessionContextProvider>
-        <Toaster richColors position="top-right" />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          
-          <Route element={<ProtectedRoute />}>
-            {/* Trang dành cho người dùng đang chờ duyệt */}
-            <Route path="/pending-approval" element={<PendingApproval />} />
-
-            {/* Các Route chỉ dành cho Admin */}
-            <Route element={<AdminRoute />}>
-              <Route element={<AppLayout />}>
-                <Route path="/admin/users" element={<UserManagement />} />
-                <Route path="/settings" element={<Settings />} />
-              </Route>
-            </Route>
-
-            {/* Các Route cho người dùng đã được duyệt */}
-            <Route element={<AppLayout />}>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/automation" element={<Automation />} />
-              <Route path="/automation/:campaignId" element={<AutomationDetail />} />
-              <Route path="/tao-video" element={<TaoVideo />} />
-            </Route>
-            <Route element={<KocLayout />}>
-              <Route path="/list-koc" element={<ListKoc />} />
-              <Route path="/list-koc/:kocId" element={<KocDetail />} />
-              <Route path="/reports" element={<Reports />} />
-              <Route path="/create-voice" element={<CreateVoicePage />} />
-              <Route path="/tao-content" element={<TaoContent />} />
-              <Route path="/video-to-script" element={<VideoToScript />} />
-            </Route>
-          </Route>
-        </Routes>
-      </SessionContextProvider>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          {/* Bạn có thể thêm các trang khác tại đây */}
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
